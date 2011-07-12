6e0e6a2c5a60f6506bc9db091831a7eef47aa983
cupidcb <bo.cui@supinfo.com>
2011-07-12 15:14:05 +0200

    Add the seed file Improve the route file, set store/index as the page d'aceuil Change the application file in the layout Add validation to model product Delete public/index.html Create store controller
    Signed-off-by: cupidcb <bo.cui@supinfo.com>

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,13 +1,13 @@
 class Product < ActiveRecord::Base
   
+  # allow display products in alphabetical order with title
+  default_scope :order => 'title'
+  
   # validation
   validates_presence_of :title, :on => :create, :message => "can't be blank"
-  validates_presence_of :image_url, :on => :create, :message => "can't be blank"
-  
-  validates_numericality_of :price, :on => :create, :message => "is not a number"
-  
-  validates_uniqueness_of :title, :on => :create, :message => "must be unique"
-  
+  validates_presence_of :image_url, :on => :create, :message => "can't be blank"  
+  validates_numericality_of :price, :on => :create, :message => "is not a number"  
+  validates_uniqueness_of :title, :on => :create, :message => "must be unique"  
   validates_format_of :image_url, :with => %r{\.(gif|jpg|png)}i, :on => :create, :message => "is invalid"
   
 end
