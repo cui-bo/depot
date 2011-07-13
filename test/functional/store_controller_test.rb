fed43c5d708b9c32c31e957536429de6fe5bdbfa
cupidcb <bo.cui@supinfo.com>
2011-07-13 11:03:36 +0200

    1. Create a new controller to handle customer-centric interactions.2. Implement the default index action.3. Add a default_scope to the Product model to specify the order to list the items on the website.4. Implement a view (an .html.erb file) and a layout to contain it (another .html.erb file).5. Use a helper to format prices the way we want. 6. Make use of a CSS stylesheet. 7. Write functional tests for our controller. ----------------------------------------------- 1. We created a Cart object in one request and were able to successfully locate the same Cart in subsequent requests using a session object,• we added a private method in the base class for all of our controllers, making it accessible to all of our controllers,• we created relationships between Carts and Line Items, relationships between Line Items and Products, and were able to navigate using these relationships, and• we added a button which caused a product to be POSTed to a cart, caus- ing a new LineItem to be created.
    Signed-off-by: cupidcb <bo.cui@supinfo.com>

--- test/functional/store_controller_test.rb
+++ test/functional/store_controller_test.rb
@@ -4,6 +4,13 @@ class StoreControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
+    
+    # test css
+    assert_select '#columns #side a', :minimum => 4
+    assert_select '#main .entry', 1
+    assert_select 'h3', 'Programming Ruby 1.9'
+    assert_select '.price', /\$[\d]+\.[\d]+/
+
   end
 
 end
